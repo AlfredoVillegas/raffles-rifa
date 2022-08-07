d31e064d11e0a233b28e6ef028bcd4c5372d8529
Alfredo Villegas <52979913+AlfredoVillegas@users.noreply.github.com>
2022-08-06 22:33:03 -0400

    Delete dist/src directory

--- dist/src/api/CheckApiEndpoint.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CheckApiEdpoints = void 0;
-const CheckApiEdpoints = (app) => {
-    app.get('/api', (req, res) => {
-        return res.status(200).send('OK');
-    });
-};
-exports.CheckApiEdpoints = CheckApiEdpoints;
